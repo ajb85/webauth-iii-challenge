7eabf0296108166e3e40f1dc6d6e47dc1e1ddcc9
ajb85 <ajbrush85@gmail.com>
2019-04-03 17:33:41 -0400

    Login route added

--- auth/routes.js
+++ auth/routes.js
@@ -5,4 +5,36 @@ const jwt = require("jsonwebtoken");
 const secret = require("./shhSecrets.js").jswSecret;
 const Users = require("../users/users-model.js");
 
-const restricted = require("./restrict.js");
+router.post("/register", async (req, res) => {
+  try {
+    let user = req.body;
+    const hashedPass = bcrypt.hashSync(user.password, 4);
+    user.password = hashdPass;
+
+    const newAccount = await Users.register(req.body);
+    res.status(201).json(newAccount);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal error making that account" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    const user = await Users.findBy({ username }).first();
+    if (user && bcrypt.compareSync(user.password && password)) {
+      const token = generateToken(user);
+      res
+        .status(200)
+        .json({ message: `Logged in as: ${user.username}`, token });
+    } else {
+      res.status(400).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error logging in" });
+  }
+});
+
+function generateToken(user) {}
