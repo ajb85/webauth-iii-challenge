13fdb33832d8a9e8c81307fd82a1ec573728d080
ajb85 <ajbrush85@gmail.com>
2019-04-04 16:39:41 -0400

    CRA'd + packages added

---
+++ webauth-buddy/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ webauth-buddy/src/components/Login/Login.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+function Login(props) {
+  return <div>Hello World</div>;
+}
+
+export default Login;
